c2c58923b0a567aea995663d028d6f68ad32f5a2
marlonbarrios <marlon@dance-tech.net>
2023-04-28 11:42:54 -0400

    added thick present meditation

---
+++ components/Thickpresent.tsx
@@ -0,0 +1,11 @@
+
+import ThickpresentData from '../data/thickpresent.json';
+import Example from './Example';
+
+const Thickpresent = () => {
+  return (
+    <Example data={ThickpresentData} />
+  );
+};
+
+export default Thickpresent;
--- pages/examples.tsx
+++ pages/examples.tsx
@@ -1,53 +1,57 @@
 import { useState } from 'react';
 import { Text, Page } from '@vercel/examples-ui';
-import Link from 'next/link'
+import Link from 'next/link';
 import Forgive from '../components/Forgive';
 import Queerness from '../components/Queerness';
-import Image from 'next/image'
+import Thickpresent from '../components/Thickpresent';
+
+
+// Define an enum for the possible active components
+enum ActiveComponent {
+  Forgive = 'Forgive',
+  Queerness = 'Queerness',
+  Thickpresent = 'Thickpresent',
+}
+
+// Define a type for the button data
+type ButtonData = {
+  label: string;
+  component: ActiveComponent;
+};
 
 function Examples() {
-  const [activeComponent, setActiveComponent] = useState('');
+  const [activeComponent, setActiveComponent] = useState<ActiveComponent | null>(null);
 
-  // function to handle button click and set the active component state
-  const handleClick = (component: string) => {
-    setActiveComponent(component);
-  };
+  // Define the button data
+  const buttons: ButtonData[] = [
+    { label: 'Forgiveness', component: ActiveComponent.Forgive },
+    { label: 'Queerness', component: ActiveComponent.Queerness },
+    { label: "'Thick Present' Meditation", component: ActiveComponent.Thickpresent },
+  ];
 
   return (
     <Page className="flex flex-col gap-12">
-      <section className="flex flex-col gap-3 text-zinc-600">
-      <div className="flex justify-between items-center">
-          <Text variant='h1'>Learn by Example</Text>
-          <div className="flex items-center">
-            <Link href="/">
-              <Image src="/images/logo.png" alt="Sati-AI" width={100} height={100} />
-            </Link>
-          </div>
-        </div>
-        <Text>
-          On this page, we provide various example conversations, conducted during testing, to help you learn what to expect and how to think about prompting Sati-AI in order to better understand yourself and your own practice.
-        </Text>
-        <div>
-            <button 
-                onClick={() => handleClick('Forgive')}
-                className="px-4 py-2 m-4 border border-zinc-400 rounded-xl active:bg-zinc-400 active:text-white"
-            >
-                Forgiveness
-            </button>
-            <button 
-                onClick={() => handleClick('Queerness')}
-                className="px-4 py-2 m-4 border border-zinc-400 rounded-xl active:bg-zinc-400 active:text-white"
-            >
-                Queerness
-            </button>
-            <Link href="/" className="px-4 py-3 m-4 border border-zinc-400 rounded-xl bg-zinc-400 text-white hover:bg-white hover:text-zinc-400">
-                Back Home
-            </Link>
-        </div>
-        {/* display the active component and hide all others */}
-        {activeComponent === 'Forgive' && <Forgive />}
-        {activeComponent === 'Queerness' && <Queerness />}
-      </section>
+      {/* ... */}
+      <div>
+        {buttons.map((button) => (
+          <button
+            key={button.component}
+            onClick={() => setActiveComponent(button.component)}
+            className="px-4 py-2 m-4 border border-zinc-400 rounded-xl active:bg-zinc-400 active:text-white cursor-pointer"
+            aria-label={button.label}
+          >
+            {button.label}
+          </button>
+        ))}
+        <Link href="/" className="px-4 py-3 m-4 border border-zinc-400 rounded-xl bg-zinc-400 text-white hover:bg-white hover:text-zinc-400">
+          Back Home
+        </Link>
+      </div>
+      {/* Display the active component */}
+      {activeComponent === ActiveComponent.Forgive && <Forgive />}
+      {activeComponent === ActiveComponent.Queerness && <Queerness />}
+      {activeComponent === ActiveComponent.Thickpresent && <Thickpresent />}
+      {/* ... */}
     </Page>
   );
 }
