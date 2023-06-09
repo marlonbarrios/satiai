216ab2597698b01930020e79f3737596cb78d5c9
marlonbarrios <marlon@dance-tech.net>
2023-06-08 22:46:15 -0400

    three  books and new links

--- pages/about.tsx
+++ pages/about.tsx
@@ -50,40 +50,36 @@ I dedicate this project with respect and gratitude to all my teachers in my inte
         </Text>
    
         <Text>I know for sure that Sati-AI is empty of a separate self. So are we.</Text>
-        
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-3/4 mx-auto text-center my-10">
-            <div>
-            <div className='mb-4'>
-                Concept, programming and prompt engineering by: 
-            </div>
-                <Link 
-                    href="https://linktr.ee/marlonbarriososolano"
-                    className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white"
-                >
-                    Marlon Barrios Solano Links
-                </Link>
-            </div>
-            <div>
-                <div className='mb-4'>
-                    Mindfulness meditation for creative technologists and artists:
-                </div>
-                <Link 
-                    href="https://marlonbarrios.github.io/mindfulness/"
-                    className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white"
-                >
-                    Contact me
-                </Link>
-            </div>
-        </div>
         </section>
-        <div className='text-center'>
-            <Link 
-                href="https://patreon.com/user?u=84024387&utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_linkv"
-                className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white"
-            >
-                Patreon
-            </Link>.
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-3/4 mx-auto text-center">
+        <div className="text-center">
+        
+        <Link 
+            href="https://linktr.ee/marlonbarriososolano"
+            className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white"
+        >
+            Marlon Barrios Solano links
+        </Link>
+    </div>
+     
+            
         </div>
+   
+        <div className="text-center">
+        <Link href="/about_a_book" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+        Sati-AI ChatBooks
+        </Link>
+      </div>
+      <div className="text-center">
+        <Link href="https://t.me/satiai_tg_bot" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+        Sati-AI in Telegram App
+        </Link>
+      </div>
+      <div className="text-center">
+        <Link href="/examples" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+          Conversation Examples
+        </Link>
+      </div>
         <div className="text-center">
             <Link href="/" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
                 Back Home
--- pages/about_a_book.tsx
+++ pages/about_a_book.tsx
@@ -34,41 +34,22 @@ height="700"></iframe>
                 </Link>
           
      
-       
+                </section>
       
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 w-3/4 mx-auto text-center my-10">
             <div>
-            <div className='mb-4'>
-                Concept, programming and prompt engineering by: 
-            </div>
+          
                 <Link 
                     href="https://linktr.ee/marlonbarriososolano"
                     className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white"
                 >
-                    Marlon Barrios Solano
+                    Marlon Barrios Solano Links
                 </Link>
             </div>
-            <div>
-                <div className='mb-4'>
-                    Mindfulness meditation for creative technologists and artists:
-                </div>
-                <Link 
-                    href="https://marlonbarrios.github.io/mindfulness/"
-                    className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white"
-                >
-                    Contact Me
-                </Link>
-            </div>
+           
         </div>
-        </section>
-        <div className='text-center'>
-            <Link 
-                href="https://patreon.com/user?u=84024387&utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_linkv"
-                className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white"
-            >
-                Patreon
-            </Link>.
-        </div>
+       
+       
         <div className="text-center">
             <Link href="/" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
                 Back Home
