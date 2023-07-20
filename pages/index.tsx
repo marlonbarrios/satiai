2d53065d2b33daabbd7706d3eac6d73b75eadd5e
marlonbarrios <marlon@dance-tech.net>
2023-07-20 13:07:21 -0400

    p5 marging

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,13 +40,7 @@ Note that Sati-AI cannot replace a trained human teacher or mental health profes
         </Link>
       </div>
     
-    <div className="text-center">
-    <Text>
-            <Link href="https://openai.com/gpt-4" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
-                Sati-Ai is powered by GPT-4
-            </Link>
-        </Text>
-        </div>
+
    
       <div className="text-center">
         <Link href="/about_a_book" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
@@ -59,21 +53,23 @@ Note that Sati-AI cannot replace a trained human teacher or mental health profes
         </Link>
       </div>
       <div className="text-center">
-        <Link href="/about_an_article" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
-        Ask me about the interview in LionsRoar
+        <Link href="/sketch" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+       Body of the Breath (art)
         </Link>
       </div>
-     
       <div className="text-center">
         <Link href="https://sati-remember.netlify.app/" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
        Remember This (Art)
         </Link>
       </div>
       <div className="text-center">
-        <Link href="/sketch" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
-       Body of the Breath (art)
+        <Link href="/about_an_article" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+        Ask me about the interview in LionsRoar
         </Link>
       </div>
+     
+      
+      
        
       <div className="text-center">
         <Link href="https://t.me/satiai_tg_bot" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
@@ -85,6 +81,13 @@ Note that Sati-AI cannot replace a trained human teacher or mental health profes
           Conversation Examples
         </Link>
       </div>
+      <div className="text-center">
+    <Text>
+            <Link href="https://openai.com/gpt-4" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+                Sati-Ai is powered by GPT-4
+            </Link>
+        </Text>
+        </div>
       <div className="text-center">
         <Link 
             href="https://linktr.ee/marlonbarriososolano"
