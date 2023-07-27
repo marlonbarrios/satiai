50c0334bd58a8d3dc44a2101c9884b609be20ef6
marlonbarrios <marlon@dance-tech.net>
2023-07-27 12:01:50 -0400

    chatvideos

---
+++ pages/about_a_video_analayo.tsx
@@ -0,0 +1,100 @@
+
+import Link from 'next/link'
+import Script from 'next/script'
+import { Text, Page } from '@vercel/examples-ui'
+
+function About() {
+  return (
+    <Page className="flex flex-col gap-12">
+      <section className="flex flex-col gap-3 text-zinc-600">
+      <iframe width="100%" height="435" src="https://www.youtube.com/embed/cd8zAVltf4s"> </iframe>
+        {/* <iframe
+          src="https://www.chatbase.co/chatbot-iframe/lJZ5UW2LEGt9ey86ByxH-"
+          width="100%"
+          height="600">
+        </iframe> */}
+
+        {/* <Script strategy="beforeInteractive">
+          {`
+            window.chatbaseConfig = {
+              chatbotId: "lJZ5UW2LEGt9ey86ByxH-",
+            }
+          `}
+        </Script> */}
+      
+        <Script 
+         id="qi0QMqp8UabLSeiK1SFiA"
+         src="https://www.chatbase.co/embed.min.js"
+          strategy="afterInteractive"
+          defer
+        />
+
+
+       
+      </section>
+      <Text className="text-center">
+    
+      Examples of Questions:
+     (Access Sati-Ai by clicking on the icon on the bottom right corner of the screen)
+      </Text>
+      <Text className="text-center">
+      Can you provide an overview of the different Buddhist traditions discussed in the interview?
+      </Text>
+      <Text className="text-center">
+      How do these traditions differ in terms of their teachings and practices?
+</Text>
+
+<Text className="text-center">
+What are some key historical developments that have shaped these traditions?
+</Text>
+<Text className="text-center">
+Are there any common threads or shared principles among these traditions?
+</Text>
+<Text className="text-center">
+How do these traditions approach mindfulness meditation?
+</Text>
+<Text className="text-center">
+What role does the concept of enlightenment play in these traditions?
+</Text>
+<Text className="text-center">
+Are there any notable figures or teachers associated with each tradition?
+</Text>
+<Text className="text-center">
+How do these traditions view the relationship between Buddhism and other religions or spiritual paths?
+</Text>
+<Text className="text-center">
+Are there any specific practices or rituals unique to each tradition?
+</Text>
+<Text className="text-center">
+How can practitioners from different traditions learn from and respect each others perspectives?
+</Text>
+
+
+      <div className="text-center">
+        <Link href="https://linktr.ee/marlonbarriososolano" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+          Marlon Barrios Solano
+        </Link>
+      </div>
+
+      <div className="text-center">
+        <Link href="https://sati-remember.netlify.app/" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+          Remember This (Ephemeral Art Object)
+        </Link>
+      </div>
+
+      <div className="text-center">
+        <Link href="https://marlonbarrios.github.io/bodyofthebreath/" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+          Body of the Breath (Ephemeral Art Object)
+        </Link>
+      </div>
+
+      <div className="text-center">
+        <Link href="/" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+          Back Home
+        </Link>
+      </div>
+    </Page>
+  )
+}
+
+export default About
--- pages/ai_augmented_videos.tsx
+++ pages/ai_augmented_videos.tsx
@@ -20,26 +20,36 @@ function About() {
           
         </div>
       <Text className="text-zinc-600">
-        We are excited to present Sati-AI ChatVideos: AI Augmented Videos. A groundbreaking project that leverages artificial intelligence to enhance your learning experience. With Sati-AI ChatVideos, we provide a conversational interface to the knowledge contained within videos. This allows you to ask questionsto Sati-Ai  and receive answers in real-time, as you watch the video. We hope that this will help you to engage more deeply with the content and to learn more effectively. Sanmple Questions are provided below each video. 
+        We are excited to present Sati-AI ChatVideos: AI Augmented Videos. A groundbreaking project that leverages artificial intelligence to enhance your learning experience. With Sati-AI ChatVideos, we provide a conversational interface to the knowledge contained within videos. This allows you to ask questionsto Sati-Ai  and receive answers in real-time, as you watch the video. We hope that this will help you to engage more deeply with the content and to learn more effectively. Sample Questions are provided below each video. 
       </Text>
-
+      
+      <Text className="text-center">
+  <Link 
+  href="/about_a_video_analayo"
+  
+>
+Bhikkhu Analayo on Respecting the Different Buddhist Traditions interviewed by Evan Hetritze
+</Link>
+</Text>
       <Text className="text-center">
       <Link 
       href="/about_a_video_varela"
   >
-     Francisco Varela
+     Francisco Varela on Art, Science, and Spirituality
   </Link>
   </Text>
 
   <Text className="text-center">
   <Link 
   href="/about_a_video_salzberg"
-  
 >
-Sharon Salzberg
+Sharon Salzberg on the Journey from Isolation to Openness
 </Link>
 </Text>
 
+<Text className="text-zinc-600">
+        Disclaimer: this is a work in prototype stage and it may have innacuracies.
+      </Text>
 
 
       <div className="text-center">
