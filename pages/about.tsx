ab53f126d835be2ca988bafdbc4b5fd2f7c74cba
marlonbarrios <marlon@dance-tech.net>
2023-07-30 21:51:56 -0400

    donation

--- pages/about.tsx
+++ pages/about.tsx
@@ -85,6 +85,13 @@ As an art and research project, Sati-AI should be approached with an open and di
         </Link>
     </div>
         <div className="text-center">
+    <Link 
+            href="https://patreon.com/user?u=84024387&utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_link"
+            className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+          Dana | Donation
+        </Link>
+    </div>
+        <div className="text-center">
             <Link href="/" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
                 Back Home
             </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -74,6 +74,13 @@ Note that Sati-AI cannot replace a trained human teacher or mental health profes
         >Marlon Barrios Solano
         </Link>
     </div>
+    <div className="text-center">
+    <Link 
+            href="https://patreon.com/user?u=84024387&utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_link"
+            className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+          Dana | Donation
+        </Link>
+    </div>
       <div className="items-center mx-auto w-80">
         <Image src="/images/logo.png" alt="Sati-AI" width={500} height={500} />
       </div>
