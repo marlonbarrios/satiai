94c1ff8ac0a63e38bca22938493d6c5fa2943cdc
marlonbarrios <marlon@dance-tech.net>
2023-08-01 14:19:47 -0400

    embeddings pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,8 +37,13 @@ Note that Sati-AI cannot replace a trained human teacher or mental health profes
       </div>
       
       <div className="text-center">
-        <Link href="/embeddings" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
-      Sati-Ai Embeddings
+        <Link href="//ai_augmented_videos" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+      Sati-Ai ChatVideos
+        </Link>
+      </div>
+      <div className="text-center">
+        <Link href="/ai_augmented_texts" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+      Sati-Ai ChatBooks
         </Link>
       </div>
       <div className="text-center">
