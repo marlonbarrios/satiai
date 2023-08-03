2572bad71dc3392ec736e43a1d6b81c926c16190
marlonbarrios <marlon@dance-tech.net>
2023-08-03 14:49:23 -0400

    on tyhe media page

--- pages/on_the_media.tsx
+++ pages/on_the_media.tsx
@@ -1,54 +1,42 @@
 import { Text, Page } from '@vercel/examples-ui'
-import { Chat } from '../components/Chat'
 import Link from 'next/link'
 import Image from 'next/image'
+import Script from 'next/script'
 function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-3">
         <div className="flex justify-between items-center"> 
-        <Text variant='h1'>Sati-Ai </Text>
+        <Text variant='h1'>Sati-Ai On The Media </Text>
          <div className="flex items-center">
             <Link href="/">
               <Image src="/images/logo.png" alt="Sati-AI" width={100} height={100} />
             </Link>
           </div>
         </div>
-        <Text className="text-zinc-600">Sati-AI is a multi-lingual AI-powered mindfulness meditation and buddhist wisdom guide that incorporates interdisciplinary connections with contemporary knowledge, such as embodied cognition and enactivism, trauma-informed approaches, and art. It primarily draws from the Theravada tradition of early Buddhism to help you cultivate mindfulness and wisdom within complex cultural histories.
-        </Text>
-        <Text className="text-zinc-600">
-Approach any dialogue with Sati-AI with openness, honesty, and a sense of humor. Ask about your meditation practice or specific verses from the Pali Canon. The tool also emphasizes the importance of embodiment and art.
-        </Text>
-        <Text className="text-zinc-600"><i>
-Note that Sati-AI cannot replace a trained human teacher or mental health professional.</i>
+        <Text className="text-zinc-600"> 
+<Link href="https://we.scienceandnonduality.com/podcasts/sounds-of-sand/episodes/2148045015">
+Remembering to Remember: Marlon Barrios Solano at Sounds of SAND/Science and Non-duality Podcast</Link>.
 </Text>
-        <Text variant="h2" className="mb-6">
-          May you be free from suffering...
-        </Text>
-        <div className="lg:w-full my-8">
-          <Chat />
-          <Text className="text-zinc-600"><i>If any statement is incomplete, please type <b>continue</b>, and Sati-AI will expand or clarify.</i></Text>
-        </div>
+
+<Text className="text-zinc-600"> 
+<Link href="https://www.lionsroar.com/ai-meditation-teacher/">
+Meet Sati-AI, a Non-Human Mindfulness Meditation Teacher on LionsRoar Magazine
+</Link></Text>
+
+
       </section>
       <div className="text-center">
         <Link href="/about" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
           Learn More
         </Link>
       </div>
-      
-      <div className="text-center">
-        <Link href="/on_the_media" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
-      Sati-Ai On The media
-        </Link>
-      </div>
-
       <div className="text-center">
         <Link href="/embeddings" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
       Sati-Ai Embeddings
         </Link>
       </div>
-      
-
+     
       <div className="text-center">
         <Link href="/contemplative_digital_art" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
         Contemplative Digital Art
@@ -85,6 +73,11 @@ Note that Sati-AI cannot replace a trained human teacher or mental health profes
           Dana | Donation
         </Link>
     </div>
+    <div className="text-center">
+            <Link href="/" className="px-8 py-2 mx-auto my-4 border border-zinc-400 rounded-xl hover:bg-zinc-400 hover:text-white">
+                Back Home
+            </Link>
+        </div>
       <div className="items-center mx-auto w-80">
         <Image src="/images/logo.png" alt="Sati-AI" width={500} height={500} />
       </div>
