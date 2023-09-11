da71b6698c168cc727271d32989aa3ab7b33abdb
andytudhope <13001517+andytudhope@users.noreply.github.com>
2023-09-11 13:25:13 -0400

    Adds AI image generation to NFT

--- packages/nextjs/components/Mint.tsx
+++ packages/nextjs/components/Mint.tsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 import getConfig from "next/config";
 import { NFTStorage } from "nft.storage";
+import { Configuration, OpenAIApi } from "openai";
 import { useMessages } from "~~/contexts/Messages";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 const { publicRuntimeConfig } = getConfig();
 
+interface ImageResponse {
+  data: ImageData[];
+}
+
+interface ImageData {
+  url: string;
+}
+
 export const Mint = () => {
   const { state, dispatch } = useMessages();
   const [loading, setLoading] = useState(false);
-  const apiKey = publicRuntimeConfig.NFT_STORAGE_KEY;
+  const [imageUrl, setImageUrl] = useState("");
+
+  const apiKey = publicRuntimeConfig.OPENAI_API_KEY;
+  const token = publicRuntimeConfig.NFT_STORAGE_KEY;
   // first, we need to clean up the conversation so it is stored in a readable and delightful form
   // we use three new lines to separate calls and responses as Sati sometimes uses \n\n itself.
   const conversation = state.messages
@@ -23,10 +35,29 @@ export const Mint = () => {
     })
     .join("\n\n\n");
 
-  // second, we need to get the image for our NFT, which will always be the same
-  async function getImage() {
-    const imageOriginUrl = "https://www.sati-ai.app/_next/image?url=%2Fimages%2Flogo.png&w=128&q=75";
-    const r = await fetch(imageOriginUrl);
+  // second, we need to get the image for our NFT, which we can generate from the conversation itself
+  async function generateImage() {
+    const configuration = new Configuration({
+      apiKey: apiKey,
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    openai
+      .createImage({
+        prompt: conversation.slice(0, 1000), //limited to the beginning so that OpenAI doesn't freak out
+        n: 1,
+        size: "512x512",
+      })
+      .then(response => {
+        const responseData = response.data as ImageResponse;
+        setImageUrl(responseData.data[0].url);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      });
+
+    const r = await fetch(imageUrl);
     if (!r.ok) {
       throw new Error("error fetching image");
     }
@@ -36,8 +67,9 @@ export const Mint = () => {
   // third, we need to store the conversation in IPFS
   const storeNFT = async () => {
     setLoading(true);
+    const image = await generateImage();
     const nft = {
-      image: await getImage(),
+      image,
       name: "Sati AI Conversation",
       description: "A mindful meditation made meaningful by your participation and presence",
       properties: {
@@ -48,7 +80,7 @@ export const Mint = () => {
     };
 
     const client = new NFTStorage({
-      token: apiKey,
+      token: token,
     });
     const metadata = await client.store(nft);
 
